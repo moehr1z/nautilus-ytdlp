9cf67a0a530912dea847a3aaf3998f67b3563f23
moehr1z <mooehr1z@gmail.com>
2022-08-07 16:13:38 +0200

    Port gui to gtk4/libadwaita

--- nautilus_ytdlp/NautilusYTDLPDialog.py
+++ nautilus_ytdlp/NautilusYTDLPDialog.py
@@ -1,47 +1,77 @@
-from gi.repository import Gtk
+import gi
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+from gi.repository import Gtk, Adw, Gio
 
 
-class NautilusYTDLPDialog(Gtk.Window):
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_default_size(400, 250)
+        self.set_title("Video Downloader")
+
+        self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+
+        self.url_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        
+
+        # configure url_box
+        self.url_box.set_spacing(10)
+        self.url_box.set_margin_top(10)
+        self.url_box.set_margin_bottom(10)
+        self.url_box.set_margin_start(10)
+        self.url_box.set_margin_end(10)
+        
+
+        #  initialize url entry
+        self.url_entry = Gtk.Entry()
+        self.url_entry.set_placeholder_text("Enter video url")
+        self.url_entry.set_hexpand(True)
+        self.url_entry.set_halign(Gtk.Align.FILL)
+
+        # initialize download button
+        self.download_button = Gtk.Button()
+        self.download_button.set_icon_name("folder-download-symbolic")
+        self.download_button.connect('clicked', self.on_download_pressed)
+        self.download_button.set_halign(Gtk.Align.END)
+
+
+        self.set_child(self.main_box)
+        self.main_box.append(self.url_box) 
+
+
+        self.url_box.append(self.url_entry)
+        self.url_box.append(self.download_button)
+        
+
+
+    def on_download_pressed(self, button):
+        pass
+
+
+#   def on_download_pressed(self, button, para):
+#         # get entered video url from entry
+#         video_urls = self.entry.get_buffer().get_text()
+
+#         # make url list
+#         video_urls = video_urls.split()
+
+#         # download every video in a seperate thread
+#         # downloaders = [(VideoDownloader(), url) for url in video_urls]
+#         # pool = [Process(target=downloader.download, args=(url, para)) for (downloader, url) in downloaders]
+#         # for p in pool:
+#         #     p.start() 
+
+#         # connection.wait(p.sentinel for p in pool)
+
+class NautilusYTDLPDialog(Adw.Application):
     """Class for the url prompt entry"""
 
-    def __init__(self):
-        super().__init__(title="Hello World")
-        self.url = ""   # the url entered, initially empty
-
-
-        super().__init__(title="Video downloader")
-        self.set_size_request(400, 100)
-        # TODO attach to main window
-
-        self.timeout_id = None
-
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
-
-        self.entry = Gtk.Entry()
-        self.entry.set_placeholder_text("Enter video url")
-        vbox.pack_start(self.entry, True, True, 0)
-
-        hbox = Gtk.Box(spacing=6)
-        vbox.pack_start(hbox, True, True, 0)
-
-        self.entry.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, "system-search-symbolic")
-         
-        self.button_download = Gtk.Button(label="Download")
-        hbox.pack_start(self.button_download, True, True, 0)
-
-
-    def on_download_pressed(self, button, para):
-        # get entered video url from entry
-        video_urls = self.entry.get_buffer().get_text()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate', self.on_activate)
         
-        # make url list
-        video_urls = video_urls.split()
+    def on_activate(self, app):
+        self.win = MainWindow(application=app)
+        self.win.present()
 
-        # download every video in a seperate thread
-        # downloaders = [(VideoDownloader(), url) for url in video_urls]
-        # pool = [Process(target=downloader.download, args=(url, para)) for (downloader, url) in downloaders]
-        # for p in pool:
-        #     p.start() 
-
-        # connection.wait(p.sentinel for p in pool)
--- nautilus_ytdlp/nautilus-ytdlp-extension.py
+++ nautilus_ytdlp/nautilus-ytdlp-extension.py
@@ -1,23 +1,23 @@
 from gi.repository import Nautilus, GObject
 import subprocess
 
-class YTDLPExtension(GObject.GObject, Nautilus.MenuProvider, Nautilus.LocationWidgetProvider):
+class YTDLPExtension(GObject.GObject, Nautilus.MenuProvider):
     """A context menu for ytdlp"""
 
     def __init__(self):
         GObject.Object.__init__(self)
 
     def menu_activate_cb(self, menu, file):
+        # Remove "file://" from uri
         path = str(file.get_uri())
         path = path.split("file://")[1]
 
+        # run main program
         args = ['nautilus-ytdlp', path] 
-        subprocess.run(args)
+        subprocess.Popen(args)
 
 
     def get_background_items(self, window, file):
-        url_prompt = self.get_widget("", window)
-
         submenu = Nautilus.Menu()
         
         item = Nautilus.MenuItem(name='YTDLPExtension::download', 
