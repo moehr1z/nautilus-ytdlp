e26ea924b89ff472de20ee9b7731a3c0bb0ce97a
moehr1z <mooehr1z@gmail.com>
2022-10-15 16:06:49 +0200

    Move main guard

--- nautilus_ytdlp/VideoDownloader.py
+++ nautilus_ytdlp/VideoDownloader.py
@@ -22,87 +22,87 @@ class VideoDownloader():
 
         
     def ydl_dwl(self, ydl):
-        ret_code = ydl.download(self.url)
-        self.ret_q.put(ret_code)
+        if __name__ == '__main__':
+            ret_code = ydl.download(self.url)
+            self.ret_q.put(ret_code)
 
 
     def download(self):
         """downloads the video corresponding to the url and sends a notification"""
-        if __name__ == '__main__':
             
-            # TODO add different codecs
-            options = {
-                'outtmpl': "%(title)s.%(ext)s",
-                'paths': {'home': self.para.path},
+        # TODO add different codecs
+        options = {
+            'outtmpl': "%(title)s.%(ext)s",
+            'paths': {'home': self.para.path},
+        }
+
+        if self.para.type == "audio":
+            extra_opt = {
+                'format': 'm4a/bestaudio/best',
+                'postprocessors': [{  # Extract audio using ffmpeg
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': self.para.format,
+                }],
             }
+        else: 
+            extra_opt = {
+                'format': "bv*[ext={0}]+ba[ext=m4a]/b[ext={0}] / bv*+ba/b".format(self.para.format),
+            }
+        
+        options.update(extra_opt)
 
-            if self.para.type == "audio":
-                extra_opt = {
-                    'format': 'm4a/bestaudio/best',
-                    'postprocessors': [{  # Extract audio using ffmpeg
-                        'key': 'FFmpegExtractAudio',
-                        'preferredcodec': self.para.format,
-                    }],
-                }
-            else: 
-                extra_opt = {
-                    'format': "bv*[ext={0}]+ba[ext=m4a]/b[ext={0}] / bv*+ba/b".format(self.para.format),
-                }
-            
-            options.update(extra_opt)
-
-            # extract title and send notification
-            with yt_dlp.YoutubeDL(options) as ydl:
-                try:
-                    self.video_info = ydl.extract_info(self.url, download=False)
-                except BaseException as err:
-                    ntfc = notifier.create_notification(
-                        title="Error downloading video",
-                        msg=repr(err),
-                        duration="long",
-                    )
-                    ntfc.show()
-                    
-                    return
-
-            ntfc = notifier.create_notification(
-                title="Downloading Video",
-                msg=self.video_info['title'],
-                duration="long",
-            )
-            ntfc.add_actions(
-                label="Cancel",
-                launch=self.cancel_download,
-            )
-            ntfc.show()
-
-            # download the video
-            with yt_dlp.YoutubeDL(options) as ydl:
-                self.proc = Process(target=self.ydl_dwl, args=(ydl,))
-                self.proc.start()
-                
-            self.proc.join()
-            code = self.ret_q.get()
-            ntfc.close()
-            if code == "cancelled":
-                ntfc = notifier.create_notification(
-                    title="Canceled download",
-                    msg=self.video_info['title'],
-                    duration="long",
-                )
-            elif code:
+        # extract title and send notification
+        with yt_dlp.YoutubeDL(options) as ydl:
+            try:
+                self.video_info = ydl.extract_info(self.url, download=False)
+            except BaseException as err:
                 ntfc = notifier.create_notification(
                     title="Error downloading video",
                     msg=repr(err),
                     duration="long",
                 )
-            else:
-                ntfc = notifier.create_notification(
-                    title="Finished download",
-                    msg=self.video_info['title'],
-                    duration="long",
-                )
+                ntfc.show()
+                
+                return
 
-            ntfc.show()
+        ntfc = notifier.create_notification(
+            title="Downloading Video",
+            msg=self.video_info['title'],
+            duration="long",
+        )
+        ntfc.add_actions(
+            label="Cancel",
+            launch=self.cancel_download,
+        )
+        ntfc.show()
 
+        # download the video
+        with yt_dlp.YoutubeDL(options) as ydl:
+            self.proc = Process(target=self.ydl_dwl, args=(ydl,))
+            self.proc.start()
             
+        self.proc.join()
+        code = self.ret_q.get()
+        ntfc.close()
+        if code == "cancelled":
+            ntfc = notifier.create_notification(
+                title="Canceled download",
+                msg=self.video_info['title'],
+                duration="long",
+            )
+        elif code:
+            ntfc = notifier.create_notification(
+                title="Error downloading video",
+                msg=repr(err),
+                duration="long",
+            )
+        else:
+            ntfc = notifier.create_notification(
+                title="Finished download",
+                msg=self.video_info['title'],
+                duration="long",
+            )
+
+        ntfc.show()
+
+        
